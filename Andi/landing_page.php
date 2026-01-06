26e5d69445d448977c35334d1716b1b09b3eb166
Anomali27 <edwardcornelius27@gmail.com>
2026-01-06 13:58:01 +0700

    php

---
+++ Andi/landing_page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Landing Page</title>
+</head>
+<body>
+    <div class="gatau">
+        <h1>
+            LANDING PAGE
+        </h1>
+    </div>
+    
+    <div class="card">
+        <ul>
+            <li>Number 1</li>
+            <li>Number 2</li>
+            <li>Number 3</li>
+            <li>Number 4</li>
+        </ul>
+    </div>
+</body>
+</html>
